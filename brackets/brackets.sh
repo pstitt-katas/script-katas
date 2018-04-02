08ae19aa313d21c3ec105f67e1ebb52ff7b2d51f
stittp <PaulStitt@Rentalcars.com>
2018-04-02 12:39:18 +0100

    Tidied up

--- brackets/brackets.sh
+++ brackets/brackets.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 isBalanced() {
 	input=$1
--- edit/convert.sh
+++ edit/convert.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 gsed 's/^\(.*\)$/<dt runat=\"server\" id=\"dt\1\">\1<\/dt>\n<dd runat=\"server\" id=\"dd\1\"><\/dd>\n/g' list
